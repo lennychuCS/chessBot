ca2ddc85f677487b628134503651eb62b552b46d
Len <lennychu69@gmail.com>
2022-01-04 13:57:10 -0600

    Updated fen to board function and display function

--- board.py
+++ board.py
@@ -9,62 +9,65 @@ class board():
         self.fen_to_board(self.fen)
         self.playerTurn = "w"
         self.castlesLeft = "KQkq"
+        self.enPassantTargets = "-"
+        self.halfMoveClock = "0"
+        self.fullMoveCount = "0"
 
     def fen_to_board(self, f):
         new_board = np.empty(shape=(8,8), dtype=str)
-        fen = f
+        fen_full = f.split(" ")
+        board_fen = fen_full[0].split("/")
+        print(board_fen)
 
-        for i in range(8):
-            temp = fen[:fen.find("/")]
-            for j in range(8):
-                try:
-                    tempInt = int(temp[0])-1
-                    if tempInt == 0:
-                        temp = temp[1:]
-                    else:
-                        temp = str(tempInt) + temp[1:]
-                except:
-                    new_board[i,j] = temp[:1]
-                    temp = temp[1:]
-            if(i != 7):
-                fen = fen[fen.find("/")+1:]
+        board_index = 0
+        charPointer = 0
+        while board_index < 64:
+            if board_index%8 == 0:
+                charPointer = 0
+            char = board_fen[int(board_index/8)][charPointer]
+            print(new_board)
+            if char.isdigit():
+                print(char)
+                charPointer += 1
+                board_index += int(char)
             else:
-                fen = fen[fen.find(" ")+1:]
+                print(char)
+                new_board[int(board_index/8)][board_index%8]=char
+                charPointer += 1
+                board_index += 1
 
         self.boardArray = new_board
+        self.playerTurn = fen_full[1]
+        self.enPassantTargets = fen_full[2]
+        self.halfMoveClock = fen_full[3]
+        self.fullMoveCount = fen_full[4]
 
     def printCurrentBoard(self):
         print(self.boardLetterToSymbol(self.boardArray))
 
     def boardLetterToSymbol(self, b):
-        nBrd = b
         # ♔♕♖♗♘♙
         # ♚♛♜♝♞♟
+        pieceMap = {
+            "k":"♔",
+            "q":"♕",
+            "r":"♖",
+            "b":"♗",
+            "n":"♘",
+            "p":"♙",
+            "K":"♚",
+            "Q":"♛",
+            "R":"♜",
+            "B":"♝",
+            "N":"♞",
+            "P":"♟",
+            "":" "
+        }
+
+        nBrd = b
         for i in range(8):
             for j in range(8):
-                if nBrd[i,j] == "r":
-                    nBrd[i,j] = "♖"
-                elif nBrd[i,j] == "n":
-                     nBrd[i,j] = "♘"
-                elif nBrd[i,j] == "b":
-                    nBrd[i,j] = "♗"
-                elif nBrd[i,j] == "q":
-                    nBrd[i,j] = "♕"
-                elif nBrd[i,j] == "k":
-                    nBrd[i,j] = "♔"
-                elif nBrd[i,j] == "p":
-                    nBrd[i,j] = "♙"
-                elif nBrd[i,j] == "R":
-                    nBrd[i,j] = "♜"
-                elif nBrd[i,j] == "N":
-                    nBrd[i,j] = "♞"
-                elif nBrd[i,j] == "B":
-                    nBrd[i,j] = "♝"
-                elif nBrd[i,j] == "Q":
-                    nBrd[i,j] = "♛"
-                elif nBrd[i,j] == "K":
-                    nBrd[i,j] = "♚"
-                elif nBrd[i,j] == "P":
-                    nBrd[i,j] = "♟"
+               nBrd[i,j] = pieceMap[nBrd[i,j]]
+
         return nBrd
 
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import board
 # ♔♕♖♗♘♙
 # ♚♛♜♝♞♟
 
-fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+fen = "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"
 
 board = board.board(fen)
 
